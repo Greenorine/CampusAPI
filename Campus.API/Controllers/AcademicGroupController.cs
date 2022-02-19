c99280a4719c608d89bd85f6bdc09dc4144c636e
Shaman <narxq@yandex.ru>
2022-02-19 18:14:21 +0500

    контроллеры групп

---
+++ Campus.API/Controllers/AcademicGroupController.cs
@@ -0,0 +1,41 @@
+﻿using Campus.Db.Entities;
+using Campus.Model.Handlers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Campus.API.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class AcademicGroupController : ControllerBase
+{
+    private readonly IMediator mediator;
+
+    public AcademicGroupController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpPost("get_group/{id}")]
+    public async Task<IActionResult> Getgroup(Guid id)
+    {
+        return Ok(await mediator.Send(new GetEntityById<AcademicGroup>(id)));
+    }
+
+    [HttpPost("add_group")]
+    public async Task<IActionResult> Addgroup([FromBody] AcademicGroup group)
+    {
+        return Ok(await mediator.Send(new UpsertEntity<AcademicGroup>(group)));
+    }
+
+    [HttpGet("remove_group/{id}")]
+    public async Task<IActionResult> Removegroup(Guid id)
+    {
+        return Ok(await mediator.Send(new DeleteEntityById<AcademicGroup>(id)));
+    }
+
+    [HttpPost("update_group")]
+    public async Task<IActionResult> Updategroup([FromBody] AcademicGroup group)
+    {
+        return Ok(await mediator.Send(new UpdateEntity<AcademicGroup>(group)));
+    }
+}
---
+++ Campus.API/Controllers/WorkGroupController.cs
@@ -0,0 +1,41 @@
+﻿using Campus.Db.Entities;
+using Campus.Model.Handlers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Campus.API.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class WorkGroupController : ControllerBase
+{
+    private readonly IMediator mediator;
+
+    public WorkGroupController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpPost("get_group/{id}")]
+    public async Task<IActionResult> Getgroup(Guid id)
+    {
+        return Ok(await mediator.Send(new GetEntityById<WorkGroup>(id)));
+    }
+
+    [HttpPost("add_group")]
+    public async Task<IActionResult> Addgroup([FromBody] WorkGroup group)
+    {
+        return Ok(await mediator.Send(new UpsertEntity<WorkGroup>(group)));
+    }
+
+    [HttpGet("remove_group/{id}")]
+    public async Task<IActionResult> Removegroup(Guid id)
+    {
+        return Ok(await mediator.Send(new DeleteEntityById<WorkGroup>(id)));
+    }
+
+    [HttpPost("update_group")]
+    public async Task<IActionResult> Updategroup([FromBody] WorkGroup group)
+    {
+        return Ok(await mediator.Send(new UpdateEntity<WorkGroup>(group)));
+    }
+}
